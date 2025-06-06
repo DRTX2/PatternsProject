86287fe104937aadf40629771bbd9027ee847605
DRTX2 <davidmanjarres2004@gmail.com>
2025-06-06 01:06:23 -0500

    Add knight attack animation

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections) )]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class Knight : MonoBehaviour
 {
 
     public float walkSpeed = 3f;
+    public float walkStopRate = 0.05f;
+    public DetectionZone attackZone;
+
     Rigidbody2D rb;
     TouchingDirections touchingDirections;
+    Animator animator;
 
     public enum WalkableDirection { Left, Right }
 
     private WalkableDirection _walkDirection;
-    private Vector2 walkDirectionVector=Vector2.right;
+    private Vector2 walkDirectionVector = Vector2.right;
 
     public WalkableDirection walkDirection
     {
@@ -21,11 +25,12 @@ public class Knight : MonoBehaviour
             if (_walkDirection != value) // flip direction
             {
                 gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
-                
+
                 if (value == WalkableDirection.Right)
                 {
-                    walkDirectionVector=Vector2.right;
-                }else if (value == WalkableDirection.Left)
+                    walkDirectionVector = Vector2.right;
+                }
+                else if (value == WalkableDirection.Left)
                 {
                     walkDirectionVector = Vector2.left;
                 }
@@ -35,10 +40,33 @@ public class Knight : MonoBehaviour
         }
     }
 
+    public bool _hasTarget = false;
+    public bool HasTarget
+    {
+        get
+        {
+            return _hasTarget;
+        }
+        private set
+        {
+
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value); // Update the animator parameter based on target presence
+        }
+    }
+
+    public bool CanMove { get { return animator.GetBool(AnimationStrings.canMove); } }
+
     private void Awake()
     {
-        rb= GetComponent<Rigidbody2D>();
-        touchingDirections= GetComponent<TouchingDirections>();
+        rb = GetComponent<Rigidbody2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HasTarget = attackZone.detectedColliders.Count > 0;
     }
 
     private void FixedUpdate()
@@ -48,10 +76,15 @@ public class Knight : MonoBehaviour
             // solo gira si ya est� caminando al menos una distancia m�nima
             //if (Mathf.Abs(rb.linearVelocity.x) > 0.1f)
             //{
-                FlipDirection();
+            FlipDirection();
             //}
         }
-        rb.linearVelocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.linearVelocity.y);
+
+        if(CanMove)
+            rb.linearVelocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.linearVelocity.y);
+        else
+            rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocity.x,0,walkStopRate ), rb.linearVelocity.y);
+
     }
 
     private void FlipDirection()
@@ -64,20 +97,10 @@ public class Knight : MonoBehaviour
         {
             walkDirection = WalkableDirection.Right;
         }
-        else { 
+        else
+        {
             Debug.LogError("Invalid walk direction set. Cannot flip direction.");
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        walkDirection = WalkableDirection.Left;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
